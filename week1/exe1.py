b10711ff1ca4618dd6abfc71c3be5ca52ac907fe
yong <yong@yong-VirtualBox.(none)>
2015-10-12 17:50:25 -0700

    add week1

---
+++ week1/exe1.py
@@ -0,0 +1,37 @@
+def getAt(str, i):
+    pass
+
+
+def find(str, c):
+    pass
+
+def getLen(str):
+    pass
+
+def test1():
+    print ("test1: ")
+    s = "hello"
+    i = 0
+    for a in s:
+         assert (a == getAt(s, i))
+        i = i + 1
+
+def test2():
+    print ("test2: ")
+    s = "hello"
+    assert (0 == find(s, 'h'))
+    assert (1 == find(s, 'e'))
+    assert (2 == find(s, 'l'))
+    assert (s[getAt(find(s, 'o'))] == 'o')
+
+def test3():
+    print ("test3:")
+    s = "hello"
+    assert (getLen(s) == 5)
+    
+
+if __name__ == "__main__":
+    test1()
+    test2()
+    test3()
+    
