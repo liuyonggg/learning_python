eaca2aad2b18b16f97c0ef89772cc9a52f973cda
yong <yong@yong-VirtualBox.(none)>
2015-11-07 20:39:06 -0800

    add quickSort

--- leetcode/sort.py
+++ leetcode/sort.py
@@ -27,12 +27,32 @@ class Solution:
                 break
         return vs
 
+    def quickSort(self, nums):
+        less = []
+        equal = []
+        bigger = []
+        if not nums:
+            return []
+        pivot = nums[0]
+        for x in nums:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            else:
+                assert (x > pivot)
+                bigger.append(x)
+        return self.quickSort(less) + equal + self.quickSort(bigger)
+
 def test_1(v, refv):
     assert (Solution().sort(v) == refv)
 
 
 def test_2(v, refv):
     assert (Solution().bubbleSort(v) == refv)
+
+def test_quicksort(v, refv):
+    assert (Solution().quickSort(v) == refv)
     
 if __name__ == "__main__":
     random.seed(0)
@@ -44,3 +64,4 @@ if __name__ == "__main__":
     print timeit.timeit(stmt='assert sorted(m) == refm', setup="from __main__ import test_1, m, refm", number=1)
     print timeit.timeit(stmt='test_1(m, refm)', setup="from __main__ import test_1, m, refm", number=1)
     print timeit.timeit(stmt='test_2(m, refm)', setup="from __main__ import test_2, m, refm", number=1)
+    print timeit.timeit(stmt='test_quicksort(m, refm)', setup="from __main__ import test_quicksort, m, refm", number=1)
