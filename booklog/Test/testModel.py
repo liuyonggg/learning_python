d611d856e6018d5e0d86ef8742f566618ac04eab
yong <yong@yong-VirtualBox.(none)>
2015-12-05 20:46:49 -0800

    Simpyfied Test

--- booklog/Test/testModel.py
+++ booklog/Test/testModel.py
@@ -2,137 +2,82 @@ import unittest
 from Model.Model import *
 
 class BookModelTest(unittest.TestCase):
+    def setUp(self):
+        self.b1 = BookModel()
+        self.b1.ID = 0
+        self.b1.name = "weirdo"
+        self.b1.author = "Bob"
+        self.b1.DoP = "2004-7-7"
+        self.b1.DoR = "2005-6-7"
+        self.b1.RoS = 0
+
+        self.b2 = BookModel()
+        self.b2.ID = 1
+        self.b2.name = "zeirdo"
+        self.b2.author = "Job"
+        self.b2.DoP = "2007-7-7"
+        self.b2.DoR = "2009-6-7"
+        self.b2.RoS = 5
+
+        self.book = BookModel()
+        self.book.ID = 0
+        self.book.name = "test"
+        self.book.author = "bob"
+        self.book.DoP = "1-1-1"
+        self.book.DoR = "1-1-1"
+        self.book.RoS = 0
+
+        self.b3 = BookModel()
+        self.b3.ID = 0
+        self.b3.name = "#Cobblestone == LIFE"
+        self.b3.author = "Ssundee"
+        self.b3.DoP = "2014-4-2"
+        self.b3.DoR = "2015-1-5"
+        self.b3.RoS = 5
+
+        self.b4 = BookModel()
+        self.b4.ID = 0
+        self.b4.name = "#Bobby is awesome!"
+        self.b4.author = "MrCrainer"
+        self.b4.DoP = "2015-8-1"
+        self.b4.DoR = "2015-8-7"
+        self.b4.RoS = 0
+
     def test_compare_by_ID(self):
-        b1 = BookModel()
-        b1.ID = 0
-        b1.name = "weirdo"
-        b1.author = "Bob"
-        b1.DoP = "2004-7-7"
-        b1.DoR = "2005-6-7"
-        b1.RoS = 0
-
-        b2 = BookModel()
-        b2.ID = 1
-        b2.name = "zeirdo"
-        b2.author = "Job"
-        b2.DoP = "2007-7-7"
-        b2.DoR = "2009-6-7"
-        b2.RoS = 5
-
-        self.assertEqual(b1.compare_by_ID(b2), -1)
-        self.assertEqual(b2.compare_by_ID(b1), 1)
-        b2.ID = 0
-        self.assertEqual(b2.compare_by_ID(b1), 0)
+        self.assertEqual(self.b1.compare_by_ID(self.b2), -1)
+        self.assertEqual(self.b2.compare_by_ID(self.b1), 1)
+        self.b2.ID = 0
+        self.assertEqual(self.b2.compare_by_ID(self.b1), 0)
 
     def test_compare_by_name(self):
-        b1 = BookModel()
-        b1.ID = 0
-        b1.name = "weirdo"
-        b1.author = "Bob"
-        b1.DoP = "2004-7-7"
-        b1.DoR = "2005-6-7"
-        b1.RoS = 0
-
-        b2 = BookModel()
-        b2.ID = 1
-        b2.name = "zeirdo"
-        b2.author = "Job"
-        b2.DoP = "2007-7-7"
-        b2.DoR = "2009-6-7"
-        b2.RoS = 5
-
-        self.assertEqual(b1.compare_by_name(b2), -1)
-        self.assertEqual(b2.compare_by_name(b1), 1)
-        b2.name = b1.name
-        self.assertEqual(b2.compare_by_name(b1), 0)
+        self.assertEqual(self.b1.compare_by_name(self.b2), -1)
+        self.assertEqual(self.b2.compare_by_name(self.b1), 1)
+        self.b2.name = self.b1.name
+        self.assertEqual(self.b2.compare_by_name(self.b1), 0)
 
     def test_compare_by_author(self):
-        b1 = BookModel()
-        b1.ID = 0
-        b1.name = "weirdo"
-        b1.author = "Bob"
-        b1.DoP = "2004-7-7"
-        b1.DoR = "2005-6-7"
-        b1.RoS = 0
-
-        b2 = BookModel()
-        b2.ID = 1
-        b2.name = "zeirdo"
-        b2.author = "Job"
-        b2.DoP = "2007-7-7"
-        b2.DoR = "2009-6-7"
-        b2.RoS = 5
-
-        self.assertEqual(b1.compare_by_author(b2), -1)
-        self.assertEqual(b2.compare_by_author(b1), 1)
-        b2.author = b1.author
-        self.assertEqual(b2.compare_by_author(b1), 0)
+        self.assertEqual(self.b1.compare_by_author(self.b2), -1)
+        self.assertEqual(self.b2.compare_by_author(self.b1), 1)
+        self.b2.author = self.b1.author
+        self.assertEqual(self.b2.compare_by_author(self.b1), 0)
 
     def test_compare_by_DoP(self):
-        b1 = BookModel()
-        b1.ID = 0
-        b1.name = "weirdo"
-        b1.author = "Bob"
-        b1.DoP = "2004-7-7"
-        b1.DoR = "2005-6-7"
-        b1.RoS = 0
-
-        b2 = BookModel()
-        b2.ID = 1
-        b2.name = "zeirdo"
-        b2.author = "Job"
-        b2.DoP = "2007-7-7"
-        b2.DoR = "2009-6-7"
-        b2.RoS = 5
-
-        self.assertEqual(b1.compare_by_DoP(b2), -1)
-        self.assertEqual(b2.compare_by_DoP(b1), 1)
-        b2.DoP = b1.DoP
-        self.assertEqual(b2.compare_by_DoP(b1), 0)
+        self.assertEqual(self.b1.compare_by_DoP(self.b2), -1)
+        self.assertEqual(self.b2.compare_by_DoP(self.b1), 1)
+        self.b2.DoP = self.b1.DoP
+        self.assertEqual(self.b2.compare_by_DoP(self.b1), 0)
 
     def test_compare_by_DoR(self):
-        b1 = BookModel()
-        b1.ID = 0
-        b1.name = "weirdo"
-        b1.author = "Bob"
-        b1.DoP = "2004-7-7"
-        b1.DoR = "2005-6-7"
-        b1.RoS = 0
-
-        b2 = BookModel()
-        b2.ID = 1
-        b2.name = "zeirdo"
-        b2.author = "Job"
-        b2.DoP = "2007-7-7"
-        b2.DoR = "2009-6-7"
-        b2.RoS = 5
-
-        self.assertEqual(b1.compare_by_DoR(b2), -1)
-        self.assertEqual(b2.compare_by_DoR(b1), 1)
-        b2.DoR = b1.DoR
-        self.assertEqual(b2.compare_by_DoR(b1), 0)
+        self.assertEqual(self.b1.compare_by_DoR(self.b2), -1)
+        self.assertEqual(self.b2.compare_by_DoR(self.b1), 1)
+        self.b2.DoR = self.b1.DoR
+        self.assertEqual(self.b2.compare_by_DoR(self.b1), 0)
 
     def test_compare_by_RoS(self):
-        b1 = BookModel()
-        b1.ID = 0
-        b1.name = "weirdo"
-        b1.author = "Bob"
-        b1.DoP = "2004-7-7"
-        b1.DoR = "2005-6-7"
-        b1.RoS = 0
-
-        b2 = BookModel()
-        b2.ID = 1
-        b2.name = "zeirdo"
-        b2.author = "Job"
-        b2.DoP = "2007-7-7"
-        b2.DoR = "2009-6-7"
-        b2.RoS = 5
-
-        self.assertEqual(b1.compare_by_RoS(b2), 1)
-        self.assertEqual(b2.compare_by_RoS(b1), -1)
-        b2.RoS = b1.RoS
-        self.assertEqual(b2.compare_by_RoS(b1), 0)
+        self.assertEqual(self.b1.compare_by_RoS(self.b2), 1)
+        self.assertEqual(self.b2.compare_by_RoS(self.b1), -1)
+        self.b2.RoS = self.b1.RoS
+        self.assertEqual(self.b2.compare_by_RoS(self.b1), 0)
 
     def test_get_ID(self):
         test = BookModel()
@@ -165,29 +110,15 @@ class BookModelTest(unittest.TestCase):
         self.assertEqual(test.get_RoS(), "0")
 
     def test_serialize(self):
-        book = BookModel()
-        book.ID = 0
-        book.name = "test"
-        book.author = "bob"
-        book.DoP = "1-1-1"
-        book.DoR = "1-1-1"
-        s1 = book.serialize()
+        s1 = self.book.serialize()
         s2 = "0\ntest\nbob\n1-1-1\n1-1-1\n0\n"
         
         self.assertEqual(s1, s2)
-        #self.assertEqual(book.serialize(), book)
 
     def test_deserialize(self):
-        book = BookModel()
         book_copy = BookModel()
-        book.ID = 0
-        book.name = "test"
-        book.author = "bob"
-        book.DoP = "1-1-1"
-        book.DoR = "1-1-1"
-        book.RoS = 0
         
-        a = book.serialize()
+        a = self.book.serialize()
         book_copy.deserialize(a)
         self.assertEqual(book_copy.ID, 0)
         self.assertEqual(book_copy.name, "test")
@@ -197,283 +128,135 @@ class BookModelTest(unittest.TestCase):
         self.assertEqual(book_copy.RoS, 0)
     
 class BookManagerModelTest(unittest.TestCase):
+    def setUp(self):
+        self.b3 = BookModel()
+        self.b3.ID = 0
+        self.b3.name = "#Cobblestone == LIFE"
+        self.b3.author = "Ssundee"
+        self.b3.DoP = "2014-4-2"
+        self.b3.DoR = "2015-1-5"
+        self.b3.RoS = 5
+
+        self.b4 = BookModel()
+        self.b4.ID = 0
+        self.b4.name = "#Bobby is awesome!"
+        self.b4.author = "MrCrainer"
+        self.b4.DoP = "2015-8-1"
+        self.b4.DoR = "2015-8-7"
+        self.b4.RoS = 0 
+   
     def test_add_book(self):
-        book = BookModel()
-        book.ID = 0
-        book.name = "test"
-        book.author = "bob"
-        book.DoP = "1-1-1"
-        book.DoR = "1-1-1"
+        self.book = BookModel()
+        self.book.ID = 0
+        self.book.name = "test"
+        self.book.author = "bob"
+        self.book.DoP = "1-1-1"
+        self.book.DoR = "1-1-1"
+        self.book.RoS = 0
         test_manager = BookManagerModel()
-        test_manager.add_book(book) 
+        test_manager.add_book(self.book) 
 
     def test_search_book_by_ID(self):
-        book = BookModel()
-        book.ID = 0
-        book.name = "#Cobblestone == LIFE"
-        book.author = "Ssundee"
-        book.DoP = "2014-4-2"
-        book.DoR = "2015-1-5"
-        book.RoS = 5
-
         test = BookManagerModel()
-        test.add_book(book)
+        test.add_book(self.b3)
         self.assertEqual(test.search_book_by_ID(0), test.books[0])
 
     def test_search_book_by_name(self):
-        book = BookModel()
-        book.ID = 0
-        book.name = "#Cobblestone == LIFE"
-        book.author = "Ssundee"
-        book.DoP = "2014-4-2"
-        book.DoR = "2015-1-5"
-        book.RoS = 5
-
         test = BookManagerModel()
-        test.add_book(book)
+        test.add_book(self.b3)
         self.assertEqual(test.search_book_by_name("#Cobblestone == LIFE"), test.books[0])
         
     def test_search_book_by_author(self):
-        book = BookModel()
-        book.ID = 0
-        book.name = "#Cobblestone == LIFE"
-        book.author = "Ssundee"
-        book.DoP = "2014-4-2"
-        book.DoR = "2015-1-5"
-        book.RoS = 5
-
         test = BookManagerModel()
-        test.add_book(book)
+        test.add_book(self.b3)
         self.assertEqual(test.search_book_by_author("Ssundee"), test.books[0])
 
     def test_search_book_by_DoP(self):
-        book = BookModel()
-        book.ID = 0
-        book.name = "#Cobblestone == LIFE"
-        book.author = "Ssundee"
-        book.DoP = "2014-4-2"
-        book.DoR = "2015-1-5"
-        book.RoS = 5
-
         test = BookManagerModel()
-        test.add_book(book)
+        test.add_book(self.b3)
         self.assertEqual(test.search_book_by_DoP("2014-4-2"), test.books[0])
 
     def test_search_book_by_DoR(self):
-        book = BookModel()
-        book.ID = 0
-        book.name = "#Cobblestone == LIFE"
-        book.author = "Ssundee"
-        book.DoP = "2014-4-2"
-        book.DoR = "2015-1-5"
-        book.RoS = 5
-
         test = BookManagerModel()
-        test.add_book(book)
+        test.add_book(self.b3)
         self.assertEqual(test.search_book_by_DoR("2015-1-5"), test.books[0])
 
     def test_search_book_by_RoS(self):
-        book = BookModel()
-        book.ID = 0
-        book.name = "#Cobblestone == LIFE"
-        book.author = "Ssundee"
-        book.DoP = "2014-4-2"
-        book.DoR = "2015-1-5"
-        book.RoS = 5
-
         test = BookManagerModel()
-        test.add_book(book)
+        test.add_book(self.b3)
         self.assertEqual(test.search_book_by_RoS(5), test.books[0])
 
     def test_sort_book_by_ID(self):
-        book1 = BookModel()
-        book1.ID = 1
-        book1.name = "#Cobblestone == LIFE"
-        book1.author = "Ssundee"
-        book1.DoP = "2014-4-2"
-        book1.DoR = "2015-1-5"
-        book1.RoS = 5
-
-        book2 = BookModel()
-        book2.ID = 0
-        book2.name = "#Bobby is awesome!"
-        book2.author = "MrCrainer"
-        book2.DoP = "2015-8-1"
-        book2.DoR = "2015-8-7"
-        book2.RoS = 0
-
         test = BookManagerModel()
-        test.add_book(book1)
-        test.add_book(book2)
+        test.add_book(self.b3)
+        test.add_book(self.b4)
         test.sort_book_by_ID()
-        books = [book1, book2]
+        books = [self.b3, self.b4]
         self.assertEqual(test.books, books)
 
     def test_sort_book_by_name(self):
-        book1 = BookModel()
-        book1.ID = 0
-        book1.name = "#Cobblestone == LIFE"
-        book1.author = "Ssundee"
-        book1.DoP = "2014-4-2"
-        book1.DoR = "2015-1-5"
-        book1.RoS = 5
-
-        book2 = BookModel()
-        book2.ID = 1
-        book2.name = "#Bobby is awesome!"
-        book2.author = "MrCrainer"
-        book2.DoP = "2015-8-1"
-        book2.DoR = "2015-8-7"
-        book2.RoS = 0
-
         test = BookManagerModel()
-        test.add_book(book1)
-        test.add_book(book2)
+        test.add_book(self.b3)
+        test.add_book(self.b4)
         test.sort_book_by_name()
-        books = [book2, book1]
+        books = [self.b4, self.b3]
         self.assertEqual(test.books, books)
 
     def test_sort_book_by_author(self):
-        book1 = BookModel()
-        book1.ID = 1
-        book1.name = "#Cobblestone == LIFE"
-        book1.author = "Ssundee"
-        book1.DoP = "2014-4-2"
-        book1.DoR = "2015-1-5"
-        book1.RoS = 5
-
-        book2 = BookModel()
-        book2.ID = 0
-        book2.name = "#Bobby is awesome!"
-        book2.author = "MrCrainer"
-        book2.DoP = "2015-8-1"
-        book2.DoR = "2015-8-7"
-        book2.RoS = 0
-
         test = BookManagerModel()
-        test.add_book(book1)
-        test.add_book(book2)
+        test.add_book(self.b3)
+        test.add_book(self.b4)
         test.sort_book_by_author()
-        books = [book2, book1]
+        books = [self.b4, self.b3]
         self.assertEqual(test.books, books)
 
     def test_sort_book_by_DoP(self):
-        book1 = BookModel()
-        book1.ID = 1
-        book1.name = "#Cobblestone == LIFE"
-        book1.author = "Ssundee"
-        book1.DoP = "2014-4-2"
-        book1.DoR = "2015-1-5"
-        book1.RoS = 5
-
-        book2 = BookModel()
-        book2.ID = 0
-        book2.name = "#Bobby is awesome!"
-        book2.author = "MrCrainer"
-        book2.DoP = "2015-8-1"
-        book2.DoR = "2015-8-7"
-        book2.RoS = 0
-
         test = BookManagerModel()
-        test.add_book(book1)
-        test.add_book(book2)
+        test.add_book(self.b3)
+        test.add_book(self.b4)
         test.sort_book_by_DoP()
-        books = [book1, book2]
+        books = [self.b3, self.b4]
         self.assertEqual(test.books, books)
 
     def test_sort_book_by_DoR(self):
-        book1 = BookModel()
-        book1.ID = 1
-        book1.name = "#Cobblestone == LIFE"
-        book1.author = "Ssundee"
-        book1.DoP = "2014-4-2"
-        book1.DoR = "2015-1-5"
-        book1.RoS = 5
-
-        book2 = BookModel()
-        book2.ID = 0
-        book2.name = "#Bobby is awesome!"
-        book2.author = "MrCrainer"
-        book2.DoP = "2015-8-1"
-        book2.DoR = "2015-8-7"
-        book2.RoS = 0
-
         test = BookManagerModel()
-        test.add_book(book1)
-        test.add_book(book2)
+        test.add_book(self.b3)
+        test.add_book(self.b4)
         test.sort_book_by_DoR()
-        books = [book1, book2]
+        books = [self.b3, self.b4]
         self.assertEqual(test.books, books)
 
     def test_sort_book_by_RoS(self):
-        book1 = BookModel()
-        book1.ID = 1
-        book1.name = "#Cobblestone == LIFE"
-        book1.author = "Ssundee"
-        book1.DoP = "2014-4-2"
-        book1.DoR = "2015-1-5"
-        book1.RoS = 5
-
-        book2 = BookModel()
-        book2.ID = 0
-        book2.name = "#Bobby is awesome!"
-        book2.author = "MrCrainer"
-        book2.DoP = "2015-8-1"
-        book2.DoR = "2015-8-7"
-        book2.RoS = 0
-
         test = BookManagerModel()
-        test.add_book(book1)
-        test.add_book(book2)
+        test.add_book(self.b3)
+        test.add_book(self.b4)
         test.sort_book_by_RoS()
-        books = [book1, book2]
+        books = [self.b3, self.b4]
         self.assertEqual(test.books, books)
 
     def test_serialize(self):
-        book1 = BookModel()
         bm = BookManagerModel()
-        book1.ID = 0
-        book1.name = "#Cobblestone == LIFE"
-        book1.author = "Ssundee"
-        book1.DoP = "2014-4-2"
-        book1.DoR = "2015-1-5"
-        book1.RoS = 5
-        bm.add_book(book1)
+        bm.add_book(self.b3)
         a = "0\n#Cobblestone == LIFE\nSsundee\n2014-4-2\n2015-1-5\n5\n"
         self.assertEqual(bm.serialize(), a) 
 
     def test_deserialize(self):
-        book1 = BookModel()
-        book1.ID = 0
-        book1.name = "#Cobblestone == LIFE"
-        book1.author = "Ssundee"
-        book1.DoP = "2014-4-2"
-        book1.DoR = "2015-1-5"
-        book1.RoS = 5
-
-        book2 = BookModel()
-        book2.ID = 1
-        book2.name = "#Bobby is awesome!"
-        book2.author = "MrCrainer"
-        book2.DoP = "2015-8-1"
-        book2.DoR = "2015-8-7"
-        book2.RoS = 0
-
         bm = BookManagerModel()
+        self.b4.ID = 1
         a = "0\n#Cobblestone == LIFE\nSsundee\n2014-4-2\n2015-1-5\n5\n1\n#Bobby is awesome!\nMrCrainer\n2015-8-1\n2015-8-7\n0\n"
         bm.deserialize(a)
-        self.assertEqual(bm.books[0].ID, book1.ID)
-        self.assertEqual(bm.books[0].name, book1.name)
-        self.assertEqual(bm.books[0].author, book1.author)
-        self.assertEqual(bm.books[0].DoP, book1.DoP)
-        self.assertEqual(bm.books[0].DoR, book1.DoR)
-        self.assertEqual(bm.books[0].RoS, book1.RoS)
-        self.assertEqual(bm.books[1].ID, book2.ID)
-        self.assertEqual(bm.books[1].name, book2.name)
-        self.assertEqual(bm.books[1].author, book2.author)
-        self.assertEqual(bm.books[1].DoP, book2.DoP)
-        self.assertEqual(bm.books[1].DoR, book2.DoR)
-        self.assertEqual(bm.books[1].RoS, book2.RoS)
+        self.assertEqual(bm.books[0].ID, self.b3.ID)
+        self.assertEqual(bm.books[0].name, self.b3.name)
+        self.assertEqual(bm.books[0].author, self.b3.author)
+        self.assertEqual(bm.books[0].DoP, self.b3.DoP)
+        self.assertEqual(bm.books[0].DoR, self.b3.DoR)
+        self.assertEqual(bm.books[0].RoS, self.b3.RoS)
+        self.assertEqual(bm.books[1].ID, self.b4.ID)
+        self.assertEqual(bm.books[1].name, self.b4.name)
+        self.assertEqual(bm.books[1].author, self.b4.author)
+        self.assertEqual(bm.books[1].DoP, self.b4.DoP)
+        self.assertEqual(bm.books[1].DoR, self.b4.DoR)
+        self.assertEqual(bm.books[1].RoS, self.b4.RoS)
 
 class TestInterator(unittest.TestCase):
     def test_next(self):
