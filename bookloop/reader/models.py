466a9551730fd9851e1b97d3e6e66d097dbfb290
zechen liu <zechen@lius-MacBook-Pro.local>
2016-01-24 20:37:59 -0800

    Model Tests Done

--- bookloop/reader/models.py
+++ bookloop/reader/models.py
@@ -1,32 +1,41 @@
 from __future__ import unicode_literals
 
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 # Create your models here.
-
+@python_2_unicode_compatible  # only if you need to support Python
 class Book(models.Model):
-    name = models.CharField(name='book name', max_length=100)
-    isbn = models.CharField(name='isbn', max_length=100)
-    authors = models.CharField(name='authors name', max_length=500)
-    pub_date = models.DateField(name='date published')
+    name = models.CharField('book name', max_length=100)
+    isbn = models.CharField('isbn', max_length=100)
+    authors = models.CharField('authors name', max_length=100)
+    pub_date = models.DateField('date published')
 
-class Recommandation(models.Model):
-    comment = models.TextField(name='comment', max_length=5000)
-    date = models.DateField(name='recommand date')
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible  # only if you need to support Python
+class Recommendation(models.Model):
+    comment = models.TextField('comment', max_length=5000)
+    date = models.DateField('recommend date')
     recipients= models.ManyToManyField(
         User,
-        through='RecommandShip',
-        through_fields=('recommandation', 'person'),
+        through='RecommendShip',
+        through_fields=('recommendation', 'to_users'),
     )
 
-class RecommandShip(models.Model):
-    recommandation = models.ForeignKey(Recommandation)
+    def __str__(self):
+        return self.comment[:25]
+
+@python_2_unicode_compatible  # only if you need to support Python
+class RecommendShip(models.Model):
+    recommendation = models.ForeignKey(Recommendation)
     to_users = models.ForeignKey(User, on_delete=models.CASCADE)
     from_user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name="recommandation initiator",
+        related_name="recommendation initiator+",
     )
-    
+    def __str__(self):
+        return self.to_users
