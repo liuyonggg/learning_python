e71c30b5ee9ef58b4150a370f3a11d7342ebb1a8
zechen liu <zechen@lius-MacBook-Pro.local>
2016-02-08 20:25:23 -0800

    Dummytest Finished

--- Dummytests/ModelTests/models.py
+++ Dummytests/ModelTests/models.py
@@ -17,3 +17,70 @@ class Answer(models.Model):
     
     def __repr__(self):
         return self.__str__()
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('XXXS', 'Really???'),
+        ('XXS', "C'mon, you can't that small... Fine, Extra Extra Small"),
+        ('XS', 'Extra Small'),
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+        ('XXL', "C'mon, you can't that large... Fine, Extra Extra Large"),
+        ('XXXL', 'Really???'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=4, choices=SHIRT_SIZES)
+
+class Manufacturer(models.Model):
+    name = models.TextField()
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    model = models.TextField()
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = (['headline'])
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "%s the place" % self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.place.name
+
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s the waiter at %s" % (self.name, self.restaurant)
--- Dummytests/ModelTests/tests.py
+++ Dummytests/ModelTests/tests.py
@@ -83,18 +83,22 @@ class ModelDummyTests(TestCase):
         self.assertEqual(q1.id, 13)
 
 #    def test_save_question(self):
- #       qt = 'what time is it 7?'
-  #      q1 = Question(text=qt)
-   #     q1.save()
-    #    q1 = Question.objects.get(text=qt)
-     #   q1.text = 'dkskfjdkfjfjs'
-      #  q1.id = 56
-       # q1.save(update_fields=['text'])
-        #q2 = Question.objects.get(text='456')
-       # self.assertEqual(q2.text, '456')
-      #  self.assertFalse(q2.id == 56)
+#       qt = 'what time is it 7?'
+#       q1 = Question(text=qt)
+ #      q1.save()
+ #      q1 = Question.objects.get(text=qt)
+   #    q1.text = 'dkskfjdkfjfjs'
+    #   q1.id = 56
+     #  q1.save(update_fields=['text'])
+      # q2 = Question.objects.get(text='456')
+      # self.assertEqual(q2.text, '456')
+      # self.assertFalse(q2.id == 56)
 
-
+    def test_foo_display_person(self):
+        p = Person.objects.create(name='Bill Cosby', shirt_size='XXXS')
+        self.assertEqual(p.name, 'Bill Cosby')
+        self.assertEqual(p.shirt_size, 'XXXS')
+        self.assertEqual(p.get_shirt_size_display(), 'Really???')
 
     def test_ordering_questions(self):
         i = 0
@@ -103,3 +107,123 @@ class ModelDummyTests(TestCase):
             i += 1
             self.assertEqual(a, b)
 
+
+    def test_foreign_key(self):
+        manufacturers = ['Toyota', 'Honda', 'Nissan', 'Porsche']
+        cars = ['SUV', 'Sedan', 'Sports Car', 'Buggy', 'Truck']
+        for i in xrange(len(manufacturers)-1):
+            m1 = Manufacturer.objects.create(name=manufacturers[i])
+            for j in xrange(len(cars)):
+                Car.objects.create(manufacturer=m1 ,model=cars[j])
+
+        for i in xrange(len(manufacturers)-1):
+            m = Manufacturer.objects.get(name=manufacturers[i])
+            cars_per_manufacturer = Car.objects.filter(manufacturer=m)
+            numbers = [0, 1, 2, 3]
+            for n in numbers:
+                cars_per_manufacturer[n].model
+            self.assertEqual(len(cars), len(cars_per_manufacturer))
+            n = 0
+            j = 0
+            while n < len(cars):
+                self.assertEqual(cars_per_manufacturer[j].model, cars[n])
+                n += 1
+                j += 1
+
+    def test_foreign_key_2(self):
+        cars = ['SUV', 'Sedan', 'Truck']
+
+        m1 = Manufacturer.objects.create(name='Toyota')
+        for i in xrange(len(cars)-1):
+            Car.objects.create(manufacturer=m1 ,model=cars[i])
+
+        for i in xrange(len(cars)-1):
+            c = Car.objects.get(model=cars[i])
+            m = Manufacturer.objects.get(car=c)
+            self.assertEqual(m.name, 'Toyota')
+
+    def test_many_to_many(self):
+        p1 = Publication(title="Bob's Awesomeness")
+        p2 = Publication(title="Bob's Life")
+        p3 = Publication(title='Bob Weekly')
+        
+        p1.save()
+        p2.save()
+        p3.save()
+
+        a1 = Article(headline="Bob's Net Worth has Surged Above $1 Billion!!!")
+
+        a1.save()
+        a1.publications.add(p1, p2, p3)
+
+        self.assertEqual(str(list(a1.publications.all())[0]), 'Bob Weekly')
+        self.assertEqual(str(list(a1.publications.all())[1]), "Bob's Awesomeness")
+        self.assertEqual(str(list(a1.publications.all())[2]), "Bob's Life")
+        self.assertEqual(str(list(p1.article_set.all())[0]), "Bob's Net Worth has Surged Above $1 Billion!!!")
+        self.assertEqual(str(list(p2.article_set.all())[0]), "Bob's Net Worth has Surged Above $1 Billion!!!")
+        self.assertEqual(str(list(p3.article_set.all())[0]), "Bob's Net Worth has Surged Above $1 Billion!!!")
+        self.assertEqual(list(p1.article_set.exclude(headline="Bob's Net Worth has Surged Above $1 Billion!!!")), [])
+
+        a2 = Article(headline='Bob and His Wife Divorced!!!')
+        a3 = Article(headline='Bob Started Funding for a New Company!!!')
+        a2.save()
+        a3.save()
+        a2.publications.add(p2)
+        a3.publications.add(p3)
+        a1.publications.remove(p2, p3)
+
+        self.assertEqual(str(list(Article.objects.filter(publications=p1))), "[<Article: Bob's Net Worth has Surged Above $1 Billion!!!>]")
+        self.assertEqual(str(list(Article.objects.filter(publications=p2))), "[<Article: Bob and His Wife Divorced!!!>]")
+        self.assertEqual(str(list(Article.objects.filter(publications=p3))), "[<Article: Bob Started Funding for a New Company!!!>]")
+
+        p4 = Publication(title='The Bobby America Magizine')
+        p4.save()
+        p4.article_set = [a1, a2, a3]
+        self.assertEqual(str(list(p4.article_set.all())), "[<Article: Bob Started Funding for a New Company!!!>, <Article: Bob and His Wife Divorced!!!>, <Article: Bob's Net Worth has Surged Above $1 Billion!!!>]")
+
+    def test_one_to_one(self):
+        p1 = Place.objects.create(name='TestR1', address='TestA1')
+        p2 = Place.objects.create(name='TestR2', address='TestA2')
+        p3 = Place.objects.create(name='TestR3', address='TestA3')
+
+        r1 = Restaurant.objects.create(place=p1, serves_hot_dogs=False, serves_pizza=False)
+        r2 = Restaurant.objects.create(place=p2, serves_hot_dogs=True, serves_pizza=True)
+
+        w1 = r1.waiter_set.create(name='Bob')
+        w2 = r1.waiter_set.create(name='Joe')
+        w3 = r1.waiter_set.create(name='Kevin')
+        w4 = r1.waiter_set.create(name='Boblina')
+        w5 = r2.waiter_set.create(name='William')
+        w6 = r2.waiter_set.create(name='Chrie')
+        w7 = r2.waiter_set.create(name='Jessica')
+        w8 = r2.waiter_set.create(name='Jessie')
+
+        w1.save()
+        w2.save()
+        w3.save()
+        w4.save()
+        w5.save()
+        w6.save()
+        w7.save()
+        w8.save()
+
+        self.assertTrue(hasattr(p1, 'restaurant'))
+        self.assertTrue(hasattr(p2, 'restaurant'))
+        self.assertFalse(hasattr(p3, 'restaurant'))
+
+        self.assertEqual(p1.restaurant, r1)
+        self.assertEqual(p2.restaurant, r2)
+
+        self.assertEqual(r1.place, p1)
+        self.assertEqual(r2.place, p2)
+
+        self.assertEqual(list(r1.waiter_set.filter(name__startswith ='J')), [w2])
+        self.assertEqual(list(r1.waiter_set.filter(name__startswith ='K')), [w3])
+        self.assertEqual(list(r1.waiter_set.filter(name__startswith ='B')), [w1, w4])
+
+        self.assertEqual(list(r2.waiter_set.filter(name__endswith ='m')), [w5])
+        self.assertEqual(list(r2.waiter_set.filter(name__endswith ='a')), [w7])
+        self.assertEqual(list(r2.waiter_set.filter(name__endswith ='e')), [w6, w8])
+
+        self.assertEqual(list(r1.waiter_set.filter(name__contains ='a')), [w4])
+        self.assertEqual(list(r2.waiter_set.filter(name__contains ='e')), [w6, w7, w8])
